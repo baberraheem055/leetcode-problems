a7222c6324e0f2eada0a9017e669462b705110f7
baberraheem055 <baberraheem055@gmail.com>
2024-01-26 12:15:01 +0500

    new file

---
+++ python tut/basic.py
@@ -0,0 +1,25 @@
+
+name = "Brook"
+print("Hello " + name)
+
+#calculation
+print(5+3)
+print(5/3)
+
+print("++++++++++++++++++++++++++++++++++++++++++++")
+import math
+
+result = math.sqrt(9)
+print(result)
+
+#or
+result = 9 ** 0.5
+print(result)
+
+#if else commands
+
+age= 32
+if age>23:
+    print("you are eliable")
+elif age==23:
+    print("not eligable")
